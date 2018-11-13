42370c70f50f68e9f19b502ed03ad02c785c3085
hooliooo <alorro3@gmail.com>
2018-11-13 22:42:57 +0100

    Initial Commit

---
+++ Villanelle/Villanelle.h
@@ -0,0 +1,19 @@
+//
+//  Villanelle.h
+//  Villanelle
+//
+//  Created by Julio Miguel Alorro on 11/13/18.
+//  Copyright © 2018 Julio Alorro Software Development, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Villanelle.
+FOUNDATION_EXPORT double VillanelleVersionNumber;
+
+//! Project version string for Villanelle.
+FOUNDATION_EXPORT const unsigned char VillanelleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Villanelle/PublicHeader.h>
+
+
